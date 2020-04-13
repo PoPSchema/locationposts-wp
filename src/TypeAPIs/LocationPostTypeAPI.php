624c7f6b2b280776d84881cc490a28dcc320c570
Leo <losoviz@gmail.com>
2020-04-13 19:15:24 +0800

    Added declare(strict_types=1);

--- src/TypeAPIs/LocationPostTypeAPI.php
+++ src/TypeAPIs/LocationPostTypeAPI.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\LocationPostsWP\TypeAPIs;
 
 use WP_Post;
--- src/TypeResolverPickers/Overrides/LocationPostContentEntityTypeResolverPicker.php
+++ src/TypeResolverPickers/Overrides/LocationPostContentEntityTypeResolverPicker.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\LocationPostsWP\TypeResolverPickers\Overrides;
 
 use PoP\PostsWP\TypeResolverPickers\ContentEntityTypeResolverPickerInterface;
